e52241dcb8e56b3347fad26b1be419714f2707f9
Piri Babayev <babayevpiri@hotmail.com>
2020-10-31 20:33:34 +0100

    Word counter is added
    It can count the words! Preliminary tests suggest we need to clean the data :D

--- data.py
+++ data.py
@@ -8,13 +8,20 @@ df = pd.read_csv('emails.csv')
 num_spam = df['label'].value_counts()[0]
 num_ham = df['label'].value_counts()[1]
 
+# Dictionary with most often occuring words for spam and ham emails
+words_spam = {} 
+words_ham= {}
+
+
 # Returns the entire dataframe
 def get_data():
     return df
 
+
 # Returns a tuple with the email and its label at the index
 def get_row(index):
     return df.at[index, 'email'], df.at[index, 'label']
+
 
 # Just for fun, returns num of words in an email at given index
 def num_words(index):
@@ -22,12 +29,39 @@ def num_words(index):
     return len(email.split())
 
 
+# Data cleaning
+def clean_data():
+    return 0
 
-# <====================== Maybe useful
 
-# Can also iterate like so, but bleh
-# Return a typle with the email and its label
-"""
-for index, row in df.iterrows():
-    row['email'], row['label']
-"""
+# Clears and then populates the dictionaries 
+def count_words():
+    words_ham.clear()
+    words_spam.clear()
+    
+    # The function that does the counting
+    def word_counter(email, words_dict):
+        for word in str(email).lower().split():
+            if word in words_dict:
+                words_dict[word] = words_dict[word] + 1
+            else:
+                words_dict[word] = 1
+    
+    # Looping through each item in the emails dataset
+    for index, row in df.iterrows():
+        # Check the label first
+        if row['label'] == 0: 
+            word_counter(row['email'], words_spam )
+        elif row['label'] == 1:
+            word_counter(row['email'], words_ham )
+        else:
+            print("Wrong label used - " + str(row['label']) + " at index " + str(index) + ".  Ignoring it." )
+
+            
+
+
+
+
+
+
+
--- svm.py
+++ svm.py
@@ -3,3 +3,19 @@ import data
 import matplotlib.pyplot as plt
 
 
+data.count_words()
+
+words_ham = sorted(data.words_ham.items(), key=lambda x: x[1], reverse = True)
+words_spam = sorted(data.words_spam.items(), key=lambda x: x[1], reverse = True)
+
+
+print("Top 10 Ham Words: ")
+
+for i in range(1,11):
+    print(str(i) + ".   " + words_ham[i][0])
+    
+    
+print("Top 10 Spam Words: ")
+
+for i in range(1,11):
+    print(str(i) + ".   " + words_spam[i][0])
