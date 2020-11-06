d6c777de975119d99f46a3c9c7f2a2ae02432342
iamSleepl3ss <aleksimm@hotmail.com>
2020-11-06 11:17:46 +0100

    Simplifying and starting a manual feature extraction for ROC problem

--- naive.py
+++ naive.py
@@ -1,18 +1,12 @@
 # Importing the data module
 import data
 import numpy as np
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.naive_bayes import BernoulliNB
-from sklearn.naive_bayes import GaussianNB
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB, GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score, roc_curve
 import matplotlib.pyplot as plt
-
-# Create a multiomail naive bayes item
-# Feed the naive bayes item top used words from each classification
 
 # Probablity of Spam
 prob_spam = data.num_spam / data.num_total
@@ -25,67 +19,96 @@ df = dfcleaned.copy()
 data.clean_data()
 
 
-#Removes stop words
+# Removes stop words
 vectorizer = CountVectorizer(stop_words='english')
 
-#Create a vectorization from all the content of email coloumn
+# Create a vectorization from all the content of email coloumn
 all_features = vectorizer.fit_transform(df['email'].apply(lambda x: np.str_(x)))
 all_features_cleaned = vectorizer.fit_transform(dfcleaned['email'].apply(lambda x: np.str_(x)))
 
-#Get possible labels
+# Get possible labels
 labels = df['label'].apply(lambda x: np.str_(x))
 
-#useless
-#feature_matrix = np.zeros((data.num_total, len(data.words_ham) + len(data.words_ham)))
+print(all_features)
+#Separates mail content and indexes it with its id for email, 3000
 
-#Split training data
+separated = df['email']
+print(separated)
+print(type(separated))
+
+
+"""
+for word in str(email).split():
+            if word in words_dict:
+                words_dict[word]= words_dict[word] + 1
+            else:
+                words_dict[word] = 1
+"""
+
+def feature_extraction(df):
+    data.count_words(False, False, False)
+    feature_matrix = np.zeros((data.num_total, len(data.words_ham) + len(data.words_ham)))
+    all_words = []
+    for index, row in df.iterrows():
+        for content in row['email']:
+            words = content.split()
+            all_words += words
+            for word in all_words:
+                for wordIndex in range(len(all_words)):
+                    feature_matrix[index,wordIndex] = all_words.count(word)
+    return feature_matrix
+
+print(feature_extraction(df))
+
+"""
+# Split training data
 X_train, X_test, y_train, y_test = train_test_split(all_features, labels, test_size=0.33, random_state=50)
 X_train_c, X_test_c, y_train_c, y_test_c = train_test_split(all_features_cleaned, labels, test_size=0.33, random_state=50)
 
-#Amount of training data, test/validation data, and combined e-mails and counted words
+# Amount of training data, test/validation data, and combined e-mails and counted words
 print("Original data features")
 print(X_train.shape, X_test.shape, all_features.shape)
 print("Cleaned data features")
 print(X_train_c.shape, X_test_c.shape, all_features_cleaned.shape)
 
-#Instantiate methods through scikit
+# Instantiate methods through scikit
 modelMNB = MultinomialNB()
 modelSVM = LinearSVC(max_iter=10000)
 modelBNB = BernoulliNB(alpha = 1.0)
 modelGNB = GaussianNB()
 
-#Instantiate methods through scikit for cleaned data
+# Instantiate methods through scikit for cleaned data
 modelMNBc = MultinomialNB()
 modelSVMc = LinearSVC(max_iter=10000)
 modelBNBc = BernoulliNB(alpha = 1.0)
 modelGNBc = GaussianNB()
 
-#Fit the model using our features.
+# Fit the model using our features.
 modelMNB.fit(X_train, y_train)
 modelSVM.fit(X_train, y_train)
 modelBNB.fit(X_train, y_train)
 modelGNB.fit(X_train.todense(), y_train)
 
-#Fit the models using cleaned data.
+# Fit the models using cleaned data.
 modelMNBc.fit(X_train_c, y_train_c)
 modelSVMc.fit(X_train_c, y_train_c)
 modelBNBc.fit(X_train_c, y_train_c)
 modelGNBc.fit(X_train_c.todense(), y_train_c)
 
 
-#Predict classification using our test data
-result1 = modelMNB.predict(X_test)
-result2 = modelSVM.predict(X_test)
-result3 = modelBNB.predict(X_test)
-result4 = modelGNB.predict(X_test.todense())
+# Predict classification using our test data
+resultMNB = modelMNB.predict(X_test)
+resultSVM = modelSVM.predict(X_test)
+resultBNB = modelBNB.predict(X_test)
+resultGNB = modelGNB.predict(X_test.todense())
 
-#Predict classification using our cleaned test data
-result1c = modelMNBc.predict(X_test_c)
-result2c = modelSVMc.predict(X_test_c)
-result3c = modelBNBc.predict(X_test_c)
-result4c = modelGNBc.predict(X_test_c.todense())
+# Predict classification using our cleaned test data
+resultMNBc = modelMNBc.predict(X_test_c)
+resultSVMc = modelSVMc.predict(X_test_c)
+resultBNBc = modelBNBc.predict(X_test_c)
+resultGNBc = modelGNBc.predict(X_test_c.todense())
 
-#Print out confusion matrix and classification report. <-- More on this later.
+# Print out confusion matrix and classification report. <-- More on this later.
 print(" ")
 print("Confusion Matrix Legend:")
 print("True Positive " + "," + "False Positive ")
@@ -97,17 +120,17 @@ print("Original data confusion matrix")
 print(" ")
 
 print("Multinomial Naive Bayes")
-print(confusion_matrix(y_test,result1))
+print(confusion_matrix(y_test,resultMNB))
 
 
 print("Bernoulli Naive Bayes")
-print(confusion_matrix(y_test,result3))
+print(confusion_matrix(y_test,resultBNB))
 
 print("Gaussian Naive Bayes")
-print(confusion_matrix(y_test,result4))
+print(confusion_matrix(y_test,resultGNB))
 
 print("Support Vector Machine")
-print(confusion_matrix(y_test,result2))
+print(confusion_matrix(y_test,resultSVM))
 
 
 print(" ")
@@ -118,16 +141,16 @@ print(" ")
 
 
 print("Multinomial Naive Bayes")
-print(confusion_matrix(y_test_c,result1c))
+print(confusion_matrix(y_test_c,resultMNB))
 
 print("Bernoulli Naive Bayes")
-print(confusion_matrix(y_test_c,result3c))
+print(confusion_matrix(y_test_c,resultBNBc))
 
 print("Gaussian Naive Bayes")
-print(confusion_matrix(y_test_c,result4c))
+print(confusion_matrix(y_test_c,resultGNBc))
 
 print("Support Vector Machine")
-print(confusion_matrix(y_test_c,result2c))
+print(confusion_matrix(y_test_c,resultSVMc))
 
 print(" ")
 print("Original data classification performance")
@@ -135,20 +158,20 @@ print(" ")
 
 print(" ")
 print("Multinomial Naive Bayes")
-print (classification_report(result1, y_test))
+print (classification_report(resultMNB, y_test))
 
 print(" ")
 print("Bernoulli Naive Bayes")
-print (classification_report(result3, y_test))
+print (classification_report(resultBNB, y_test))
 
 print(" ")
 print("Gaussian Naive Bayes")
-print (classification_report(result4, y_test))
+print (classification_report(resultGNB, y_test))
 #https://stackoverflow.com/questions/40659212/futurewarning-elementwise-comparison-failed-returning-scalar-but-in-the-futur
 
 print(" ")
 print("Support Vector Machine")
-print (classification_report(result2, y_test))
+print (classification_report(resultSVM, y_test))
 
 print(" ")
 print(" ")
@@ -158,17 +181,80 @@ print(" ")
 
 print(" ")
 print("Multinomial Naive Bayes")
-print (classification_report(result1c, y_test_c))
+print (classification_report(resultMNB, y_test_c))
 
 print(" ")
 print("Bernoulli Naive Bayes")
-print (classification_report(result3c, y_test_c))
+print (classification_report(resultBNBc, y_test_c))
 
 print(" ")
 print("Gaussian Naive Bayes")
-print (classification_report(result4c, y_test_c))
+print (classification_report(resultGNBc, y_test_c))
 #https://stackoverflow.com/questions/40659212/futurewarning-elementwise-comparison-failed-returning-scalar-but-in-the-futur
 
 print(" ")
 print("Support Vector Machine")
-print (classification_report(result2c, y_test_c))
+print (classification_report(resultSVMc, y_test_c))
+
+
+# Compute ROC curve and ROC area for each method used
+# Baseline
+r_prob = [0 for _ in range(len(y_test))]
+
+# Get the probability of the predictions
+mnnb_prob = modelMNB.predict_proba(X_test)
+bnb_prob = modelBNB.predict_proba(X_test)
+gnnb_prob = modelGNB.predict_proba(X_test.todense())
+svm_prob = modelSVM.decision_function(X_test)
+
+# Keep positive outcome of the methods
+mnnb_prob = mnnb_prob[:, 1]
+bnb_prob = bnb_prob[:, 1]
+gnnb_prob = gnnb_prob[:, 1]
+#svm_prob = svm_prob[:, 1]
+
+print(mnnb_prob.shape)
+print(gnnb_prob.shape)
+
+# Compute the AUROC, area under receiver operating characteristic
+r_auc = roc_auc_score(y_test, r_prob)
+mnnb_auc = roc_auc_score(y_test, mnnb_prob)
+bnb_auc = roc_auc_score(y_test, bnb_prob)
+gnnb_auc = roc_auc_score(y_test, gnnb_prob)
+svm_auc = roc_auc_score(y_test, svm_prob)
+
+
+#print(r_auc)
+print(mnnb_auc)
+print(gnnb_auc)
+
+y_test= '1' <= y_test
+
+# Get false positives and true positives to be plotted on the ROC curve
+r_fpr, r_tpr, _ = roc_curve(y_test, r_prob)
+mnnb_fpr, mnnb_tpr, mnnb_threshold = roc_curve(y_test, mnnb_prob)
+bnb_fpr, bnb_tpr, bnb_threshold = roc_curve(y_test, bnb_prob)
+gnnb_fpr, gnnb_tpr, gnnb_threshold = roc_curve(y_test, gnnb_prob)
+svm_fpr, svm_tpr, svm_threshold = roc_curve(y_test, svm_prob)
+
+print(gnnb_fpr)
+print(gnnb_tpr)
+print(mnnb_fpr)
+print(mnnb_tpr)
+print(mnnb_threshold.shape)
+print(bnb_threshold.shape)
+print(gnnb_threshold.shape)
+print(svm_threshold.shape)
+
+
+plt.plot(r_fpr, r_tpr, linestyle="--")
+plt.plot(mnnb_fpr, mnnb_tpr, marker='.', label="Multinomial Naive Bayes, " % mnnb_auc)
+plt.plot(bnb_fpr, bnb_tpr, marker='.', label="Bernoulli Naive Bayes, " % mnnb_auc)
+plt.plot(gnnb_fpr, gnnb_tpr, marker='.', label="Gaussian Naive Bayes, " % mnnb_auc)
+plt.plot(svm_fpr, svm_tpr, marker='.', label="Support Vector Machine, " % mnnb_auc)
+plt.title("ROC plot")
+plt.xlabel("False Positive Rate (1 - Specificity)")
+plt.ylabel("True Positive Rate (Sensitivity)")
+plt.legend()
+plt.show()
+"""
---
+++ perceptron.py
@@ -0,0 +1,204 @@
+import data, sys
+import numpy as np
+from sklearn.model_selection import train_test_split
+import pandas as pd
+#import seaborn as sn
+import matplotlib.pyplot as plt
+
+#------------------ function definitions ------------------
+# Function that merges the email and lable tuples into a dictionary
+def create_dictionary(x, y):
+    dictionary = {}
+    if len(x) == len(y):
+        for i in x.keys():
+            dictionary.update({x[i]: y[i]})
+    return dictionary
+
+# Function for perceptron training
+def learn_weights(weights, training_set, num_iterations, learning_constant, threshold = 0, verbose = True):
+    if verbose: print(f"The training iterates {num_iterations} times with a learning constant of {learning_constant}\n")
+    least_sum = float('inf')
+    most_sum = float('-inf')
+    # Iterating the weight adjustment process
+    for i in range(num_iterations):
+        # For each instance in the training set
+        for instance in training_set.keys():
+            weight_sum = 0.0
+            # Loop through each word in the instance
+            for word in instance.split():
+                # Add the word to the weights dictionary if it's not present
+                if word not in weights.keys(): weights.update({word: 0.0})
+                # Find the ham and spam frequency for this word in the datasett
+                f_spam = words_spam[word] if word in words_spam.keys() else 0.0
+                f_ham = words_ham[word] if word in words_ham.keys() else 0.0
+                # Add the weighted frequency of ham and/or spam to the sum. If the
+                # word is more frequent in ham than spam, then the value will be negative
+                weight_sum += weights[word] * (f_spam-f_ham)
+            # On the last iteration, the lowest and highest weight sum is found.
+            # These weight sums will be used to determin the thresholds used for
+            # the ROC curve.
+            if i == num_iterations-1:
+                if weight_sum < least_sum: least_sum = weight_sum
+                if weight_sum > most_sum: most_sum = weight_sum
+            # The perceptron is excited (1) when the sum of weights is more than
+            # 0 (spam) and not excited if the sum of weights is less than  or 
+            # equal to 0 (not spam)
+            perceptron_output = 1 if weight_sum > threshold else 0
+            # Update the weights of the relavant words to the instance at hand
+            # based on the perceptron training rule
+            for word in instance.split():
+                f_spam = words_spam[word] if word in words_spam.keys() else 0.0
+                f_ham = words_ham[word] if word in words_ham.keys() else 0.0
+                weights[word] += float(learning_constant) * float(training_set[instance]-perceptron_output) * float((f_spam-f_ham))
+    return least_sum, most_sum
+             
+# Function to apply the weights to an instance
+def apply(weights, instance, threshold = 0):
+    weight_sum = 0.0
+    # Works similarly to the learning function, where the sum of the
+    # weights gets summed up, and if the value is positive, then the
+    # perceptron is excited (1)
+    for word in instance.split():
+        if word not in weights.keys(): weights.update({word: 0.0})
+        f_spam = words_spam[word] if word in words_spam.keys() else 0.0
+        f_ham = words_ham[word] if word in words_ham.keys() else 0.0
+        weight_sum += weights[word] * (f_spam-f_ham)
+    if weight_sum > threshold: return 1
+    return 0
+
+# Function to test the weights on a testing set
+def test_weights(weights, testing_set):
+    tp, tn, fp, fn = 0, 0, 0, 0
+    # Each instance in the testing set has the learned weights
+    # applied to it.
+    for instance in testing_set.keys():
+        guess = apply(weights, instance)
+        # The perceptron result is then checked with the labeling
+        if guess == testing_set[instance]:
+            # If the perceptron result is correct, then either the
+            # true positive or true negative value is incremented
+            if guess == 0: tp += 1
+            else: tn += 1
+        else:
+            # If the perceptron result is incorrect, then either
+            # the false positive or false negative value is
+            # incremented
+            if guess == 0: fp += 1
+            else: fn += 1
+    return tp, tn, fp, fn
+
+# Function to set the iteration and learning constant parameters
+def set_params():
+    # Setting the training parameters to be 20 iterations and a learning constant 
+    # of 0.1 by default. These may be changed by providing them when starting the 
+    # python script
+    itr = int(sys.argv[2]) if len(sys.argv) >= 3 else 20
+    lc = float(sys.argv[3]) if len(sys.argv) >= 4 else 0.1
+    return itr, lc
+
+# Function that plots a confusion matrix
+def plot_cm(cm):
+    df_cm = pd.DataFrame(cm, index = ["Real Ham", "Real Spam"], columns = ["Guessed Ham", "Guessed Spam"])
+    sn.heatmap(df_cm, annot=True, fmt="d", cmap=sn.color_palette("rocket_r", as_cmap=True))
+    plt.show()
+
+# Function that plots an ROC curve
+def plot_roc(num_points, lowest_threshold, highest_threshold, training_set, testing_set):
+    print(f"Creating the ROC graph with {num_points} points. This may take a while...")
+    # Get the iteration and learning constant parameters
+    itr, lc = set_params()
+    # Initiate the arrays that will hold the true positive and true 
+    # negative rates
+    tp_rates = [0.0]
+    fp_rates = [0.0]
+    # Calculate the threshold step based on the number of points wanted in
+    # the ROC curve
+    step = (abs(lowest_threshold) + abs(highest_threshold)) / (num_points+1)
+    # Initiate the thresholde to be the lowest threshold
+    current_threshold = lowest_threshold
+    # Iterate num_points times to get confusion matricies with different 
+    # thresholds
+    for i in range(num_points):
+        # Step
+        current_threshold += step
+        # Create new weights
+        weights = {}
+        # Run the training function with the new threshold
+        learn_weights(weights, training_set, itr, lc, current_threshold, False)
+        # Test the new weights and get the confusion matrix for that threshold
+        tp, tn, fp, fn = test_weights(weights, testing_set)
+        # Append the true positive and false positive rates to the arrays
+        tp_rates.append(tp/(tp+fn))
+        fp_rates.append(fp/(fp+tn))
+    # Finish the curve off at the point 1,1
+    tp_rates.append(1)
+    fp_rates.append(1)
+    # Run the orer_points function to get a nice curve. Without this funciton
+    # the curve intersects itself as the thresholds at both low and high extremes
+    # produce similar results
+    order_points(fp_rates, tp_rates)
+    # Plot the curve using matplotlib and show it.
+    plt.title('ROC curve')
+    plt.plot([0, 1], [0, 1], 'r--', zorder=1)
+    plt.plot(fp_rates, tp_rates, color='blue', zorder=2)
+    plt.scatter(fp_rates, tp_rates, color='red', zorder=3)
+    plt.xlabel('False positive rate')
+    plt.ylabel('True positive rate')
+    plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
+    plt.show()
+    
+# Dumb function that fixes the order of the points
+def order_points(x, y):
+    points = []
+    # Create pairs of x,y coordinates
+    for i in range(0, len(x)):
+        points.append([x[i], y[i]])
+    # Sort them in ascending x value order
+    points.sort()
+    # Redistribute the coordinates back to their
+    # respective arrays
+    for i in range(0, len(x)):
+        x[i] = points[i][0]
+        y[i] = points[i][1]
+
+
+#-------------------- running the code --------------------
+# Getting the dataset from data.py
+data.count_words()
+df = data.get_data()
+words_ham, words_spam = data.order_words()
+x_train, x_test, y_train, y_test = train_test_split(df['email'], df['label'], test_size = 0.333, random_state = 50)
+# Converting the data from data.py into dictionaries. This choice was done mainly
+# because the perceptron seemed easier to implement with the use of dictionaries.
+words_ham, words_spam = dict(words_ham), dict(words_spam)
+training_set = create_dictionary(x_train, y_train)
+testing_set = create_dictionary(x_test, y_test)
+
+# The weight dictionary, as well as the iteration nummber and the learning 
+# constant are instanciated, before being sent into the learn_weights function
+weights = {}
+itr, lc = set_params()
+
+# Running the training algorithm with the provided parameters.
+lowest_threshold, highest_threshold = learn_weights(weights, training_set, itr, lc)
+# The test function returns the four values ov a confusion matrix
+tp, tn, fp, fn = test_weights(weights, testing_set)
+
+# Calculating the accuracy, true positive- and false positive rate of 
+# the method at threshold 0
+acc = (tp+tn)/(tp+tn+fp+fn)*100
+tp_rate = tp/(tp+fn)
+fp_rate = fp/(fp+tn)
+
+# Printing the data to terminal
+print("Accuracy of the model at threshold 0: %.1f" % acc + "%")
+print("True positive rate: %.2f" % tp_rate)
+print("False positive rate: %.2f\n" % fp_rate)
+
+# Plotting the confusion matrix of a perceptron with the threshold set to 0
+plot_cm([[tp, fp],[fn, tn]])
+
+# Instanciating the number of points the ROC curve will have
+points_num = int(sys.argv[1]) if len(sys.argv) >= 2 else 10
+# Plotting the ROC curve.
+plot_roc(points_num, lowest_threshold, highest_threshold, training_set, testing_set)
