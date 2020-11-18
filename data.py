23dc5d8678117bd57c3fdf603b97ac70fd2e3c25
Piri Babayev <babayevpiri@hotmail.com>
2020-11-18 20:28:28 +0100

    Enron emails can now be accessed (read desc)
    the funtion get_big_df() will return a dataframe with all Enron emails, stipped of punctuation, lower cased, and url, numbers replaced as words url and number.
    
    Takes some time.
    
    NB: the new argument in count_words should point to the db of your choice. big_df is the one you want. The count_words will still populate words_spam and words_ham, and clean everything that was there before.

--- data.py
+++ data.py
@@ -1,16 +1,68 @@
 # Data retrieval
 import pandas as pd
+import os
+
+# Natural language processing tookit
 import nltk.stem 
 from nltk.corpus import stopwords
 
+import string
+import re
 
 # If any of the nltk libraries are not already downloaded, uncomment this
 #nltk.download('wordnet')
 #nltk.download('stopwords')
 
-# Read the csv file with all the emails
+# Read the csv file with all the Spamassassin emails
 # All email are read into a pandas dataframe
 df = pd.read_csv('data/emails.csv')
+
+# Enron database 
+
+# Returns the Enron db
+# lower cased, punctuation and newlines removed, urls and numbers replaces to words "url" and "number"
+def get_big_df():
+    
+    def is_number(n):
+        try:
+            float(n)   
+        except ValueError:
+            return False
+        return True
+
+    def pre_clean(email):
+        #return email
+        email = email.lower().replace('\n', ' ')
+        
+        # The regex is from https://daringfireball.net/2010/07/improved_regex_for_matching_urls 
+        # Swaps links with the word "url"
+        url = re.compile(r"""(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""")    
+        email = url.sub(r'url', email)
+    
+        temp = ""
+
+        # Replaces all numbers with the word "number"
+        for word in email.split():
+            if is_number(word):
+                word = "number"
+
+            temp = temp + word + " "
+        
+        return temp.translate(temp.maketrans("","", string.punctuation))
+
+    # Create a new dataframe that will be returned
+    big_df = pd.DataFrame(columns=('email', 'label'))
+    
+    # Adding all
+    for entry in os.scandir(r'data/Enron/ham'):
+        big_df = big_df.append( pd.DataFrame( [[pre_clean(open(entry).read()), 0]] , columns = ['email', 'label']), ignore_index=True )  # yeah thats how append works in Pandas....
+    
+    for entry in os.scandir(r'data/Enron/spam'):
+        big_df = big_df.append( pd.DataFrame( [[pre_clean(open(entry).read()), 1]] , columns = ['email', 'label']), ignore_index=True )
+    
+    return big_df
+
+big_df = get_big_df()
 
 # The stop words from nltk library has the most common words used in a language that bear no meaning (articles, prepositions, etc.)
 stop_words = set(stopwords.words('english'))
@@ -50,13 +102,13 @@ def num_words(index):
     # 4. Lemmatizes the words.
     # 5. Stemming 
     
-def clean_data(max_length = 1, lemmatize = True, stem = True):
+def clean_data(max_length = 1, lemmatize = True, stem = True, database = df):
     temp = ""
     lemmatizer = nltk.stem.WordNetLemmatizer()
     stemmer = nltk.stem.PorterStemmer()
     
     # Loop through emails
-    for index, row in df.iterrows():
+    for index, row in database.iterrows():
 
         # Lower case the entire email
         email_lc = str(row['email']).lower()
@@ -85,14 +137,14 @@ def clean_data(max_length = 1, lemmatize = True, stem = True):
         
 
         # Rewrite the original email 
-        df.at[index, 'email'] = temp
+        database.at[index, 'email'] = temp
         temp = ""
 
 # Clears and then populates the dictionaries 
-def count_words(data_cleaning = True, lemmatize = True, stem = True):
+def count_words(data_cleaning = True, lemmatize = True, stem = True, database = df):
     
     if data_cleaning:
-        clean_data(data_cleaning, lemmatize, stem)
+        clean_data(data_cleaning, lemmatize, stem, database)
     
     words_ham.clear()
     words_spam.clear()
@@ -107,7 +159,7 @@ def count_words(data_cleaning = True, lemmatize = True, stem = True):
                 words_dict[word] = 1
 
     # Looping through each item in the emails dataset
-    for index, row in df.iterrows():
+    for index, row in database.iterrows():
         # Check the label first
         if row['label'] == 1: 
             word_counter(row['email'], words_spam )
@@ -123,9 +175,3 @@ def count_words(data_cleaning = True, lemmatize = True, stem = True):
 def order_words():
     return sorted(words_ham.items(), key=lambda x: x[1], reverse = True) , sorted(words_spam.items(), key=lambda x: x[1], reverse = True)
     
-count_words(True, True, False)
-
-ham, spam = order_words()
-
-print(ham[0:20])
-print(spam[0:20])
